357466015de08276946c98ad05fe3935887d2fd9
Moésio Filho <moesio.f@gmail.com>
2022-07-28 12:58:29 -0300

    feat: add imp to simulator layer

---
+++ src/main/java/br/ufrpe/mips/simulator/imp/MIPS32Processor.java
@@ -0,0 +1,72 @@
+package br.ufrpe.mips.simulator.imp;
+
+import java.util.Map;
+
+import br.ufrpe.mips.data.IMemoryManager;
+import br.ufrpe.mips.simulator.IMIPS32;
+
+/**
+ * Essa classe representa um processador MIPS32.
+ * 
+ * @version 1.0
+ */
+public class MIPS32Processor implements IMIPS32 {
+
+  // Memória principal e registradores
+  private IMemoryManager memory;
+
+  @Override
+  public void reset() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void loadData(Map<Integer, Integer> data) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void loadMemory(Map<Integer, Integer> mem) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void loadRegisters(Map<String, Integer> regs) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void runInstruction(String hexInstruction) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public String output() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String toAssembly() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Map<String, Integer> registers() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Map<Integer, Integer> memory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
