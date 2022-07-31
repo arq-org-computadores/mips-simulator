c682170546dc73358388199cb5cb80e80f4b332f
Moésio Filho <moesio.f@gmail.com>
2022-07-31 13:39:38 -0300

    feat: update WordMemoryLocation
    - Add initial implementation;

--- src/main/java/br/ufrpe/mips/data/imp/WordMemoryLocation.java
+++ src/main/java/br/ufrpe/mips/data/imp/WordMemoryLocation.java
@@ -1,5 +1,7 @@
 package br.ufrpe.mips.data.imp;
 
+import java.nio.ByteBuffer;
+import java.util.Objects;
 import br.ufrpe.mips.data.IMemoryLocation;
 import br.ufrpe.mips.data.utils.MemoryLocationType;
 
@@ -10,41 +12,54 @@ import br.ufrpe.mips.data.utils.MemoryLocationType;
  */
 public final class WordMemoryLocation implements IMemoryLocation<Integer> {
 
-  // Internamente, essa classe manipula 4 localizações de memória
-  // de 1 byte.
-  private ByteMemoryLocation cell0;
-  private ByteMemoryLocation cell1;
-  private ByteMemoryLocation cell2;
-  private ByteMemoryLocation cell3;
+  private final ByteMemoryLocation cell0;
+  private final ByteMemoryLocation cell1;
+  private final ByteMemoryLocation cell2;
+  private final ByteMemoryLocation cell3;
+
+  public WordMemoryLocation(ByteMemoryLocation c0, ByteMemoryLocation c1, ByteMemoryLocation c2,
+      ByteMemoryLocation c3) {
+    this.cell0 = Objects.requireNonNull(c0);
+    this.cell1 = Objects.requireNonNull(c1);
+    this.cell2 = Objects.requireNonNull(c2);
+    this.cell3 = Objects.requireNonNull(c3);
+  }
 
   @Override
   public boolean isReserved() {
-    // TODO Auto-generated method stub
-    return false;
+    return this.cell0.isReserved();
   }
 
   @Override
   public MemoryLocationType type() {
-    // TODO Auto-generated method stub
-    return null;
+    return this.cell0.type();
   }
 
   @Override
   public int address() {
-    // TODO Auto-generated method stub
-    return 0;
+    return this.cell0.address();
   }
 
   @Override
   public Integer read() {
-    // TODO Auto-generated method stub
-    return null;
+    return ByteBuffer.wrap(this.asByteArray()).getInt();
   }
 
   @Override
   public void write(Integer content) {
-    // TODO Auto-generated method stub
-
+    ByteBuffer buffer = ByteBuffer.allocate(4);
+    buffer.putInt(content);
+    this.updateFromByteArray(buffer.array());
   }
 
+  private byte[] asByteArray() {
+    return new byte[] {this.cell0.read(), this.cell1.read(), this.cell2.read(), this.cell3.read()};
+  }
+
+  private void updateFromByteArray(byte[] arr) {
+    this.cell0.write(arr[0]);
+    this.cell1.write(arr[1]);
+    this.cell2.write(arr[2]);
+    this.cell3.write(arr[3]);
+  }
 }
