0115a4284527c99dbfc3dd7048d49fbb60dd3673
Lucas Henrique Gonzaga de Sales <lucas7henrique@gmail.com>
2022-08-08 00:32:21 -0300

    feat: function MFLO

--- src/main/java/br/ufrpe/mips/simulator/utils/disassembler/MIPSDisassembler.java
+++ src/main/java/br/ufrpe/mips/simulator/utils/disassembler/MIPSDisassembler.java
@@ -124,6 +124,7 @@ public final class MIPSDisassembler {
     return switch (instruction) {
       case DIV -> "div $%d, $%d".formatted(r.rs(), r.rt());
       case MULT -> "mult $%d, $%d".formatted(r.rs(), r.rt());
+      case MFLO -> "mflo $%d".formatted(r.rd());
       case ADD -> "add $%d, $%d, $%d".formatted(r.rd(), r.rs(), r.rt());
       case SUB -> "sub $%d, $%d, $%d".formatted(r.rd(), r.rs(), r.rt());
       case ADDI -> "addi $%d, $%d, %d".formatted(i.rt(), i.rs(), i.immediate());
