4077be1bee7f0dcdc894a8b1b54c539c76712b05
Lucas Henrique Gonzaga de Sales <lucas7henrique@gmail.com>
2022-08-08 00:48:42 -0300

    feat: ADD MFHI

--- src/main/java/br/ufrpe/mips/simulator/utils/operation/ArithmeticLogic.java
+++ src/main/java/br/ufrpe/mips/simulator/utils/operation/ArithmeticLogic.java
@@ -318,5 +318,16 @@ public class ArithmeticLogic {
 
     d.write((int) v1);
   }
+
+  public void MFHI(AssemblyInstruction instruction, StringBuffer buffer) {
+
+    RField rField = instruction.fields().asRField();
+
+    IRegister d = this.memory.getRegisterFromNumber(rField.rd());
+
+    long v1 = this.memory.getHI().read();
+
+    d.write((int) v1);
+  }
 }
 
